f302f94a045d38e66b881a46e2981f3e137ddc8a
Santiago9589 <santiagodejesus587@gmail.com>
2022-04-27 23:31:40 -0300

    Ejercicio 4

---
+++ Ejercicio 4/index.js
@@ -0,0 +1,29 @@
+for (let i = 0 ; i<=9;i++){
+
+    switch(i){
+        case 2:
+            console.log(`Número primo`);
+        break;
+        case 3:
+            console.log(`Número primo`);
+        break;
+        case 5:
+            console.log(`Número primo`);
+        break;
+        case 7:
+            console.log(`Número primo`);
+        break;
+        default:
+            console.log(`iteracion numero ${i}`);
+        break;
+    }
+    /*
+    
+    if(i%2===0){
+        console.log(`el numero ${i} es par`)
+    }else{
+        console.log(`iteracion numero ${i}`)
+    }*/
+
+    
+}
