e0206028caca4db2807077d79c35720287a2b578
Santiago9589 <santiagodejesus587@gmail.com>
2022-05-01 13:03:30 -0300

    Ejercicio 8

---
+++ Ejercicio 8/index.js
@@ -0,0 +1,26 @@
+/*
+
+¿Qué beneficios nos trae el debugging?
+
+Indica cuál de los siguientes elementos son clave en el debugging.
+
+Inspección de variables.
+
+Análisis del código tras encontrar un error.
+
+Ejecución paso a paso.
+
+La eficiencia de la ejecución del código.
+
+Las tres primeras respuestas son correctas
+
+
+
+
+
+
+Respuesta.
+
+Las tres primeras respuestas son correctas
+
+*/
